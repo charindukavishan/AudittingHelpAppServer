d515787da6045755a0fe321f946b49ebdcf9ffd3
charindukavishan <charindukavishan@gmail.com>
2019-02-01 22:08:27 +0530

    skfjghjsfg

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'charindu123',
   database : 'university'
 });
 connection.connect(function(err){
